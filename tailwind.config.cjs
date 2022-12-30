9da329c0d6262a571fe10a1e6faea98a8e49a2cd
fabricio <fabricioffa08@gmail.com>
2022-12-30 16:23:23 -0300

    fix-css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,7 +24,7 @@ module.exports = {
         }
       },
       boxShadow: {
-        card: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;'
+        card: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px'
       }
     },
   },
